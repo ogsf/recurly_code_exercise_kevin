23a776d3060975c5359545f8b097b853bd175c25
Kevin Tinkler <kevint@boulder.net>
2016-03-14 16:48:11 -0600

    minor formatting changes

--- tests/api/recurly_account_api.rb
+++ tests/api/recurly_account_api.rb
@@ -6,11 +6,12 @@
 # bundle exec ruby tests/api/recurly_account_api.rb
 #
 # - Kevin Tinkler 3/13/16
-#
+
 require 'rubygems'
 require 'recurly'
 require 'yaml'
 
+#Return nested hash of data from yaml file
 @data = YAML.load_file("data/data.yml")
 api_auth = @data["api_kevint"]
 
@@ -21,8 +22,10 @@ Recurly.default_currency  = api_auth["default_currency"]
 
 begin
 
-  #Create an account.
+  #Clean up first part of data hash reference
   account_data = @data["api_account_1"]
+
+  #Create an account.
   account = Recurly::Account.create(
     :account_code => @unique_account_code,
     :email        => account_data["email"],
@@ -40,11 +43,12 @@ begin
   )
 
   puts account.response.inspect #Looking at the HTTP Status Code
-  puts                          #empty line to space output
+  puts                          #empty line to space output in terminal display
 
   #Check if the new account exists
-  #if account does not exist, a NotFound Error will be thrown
   account = Recurly::Account.find @unique_account_code
+
+  #if account does not exist, a NotFound Error will be thrown
   # account = Recurly::Account.find '54321'  #Uncomment to check the NotFound Error
   puts "Account: #{account.inspect}"
   rescue Recurly::Resource::NotFound => e

408a3aa72f05966a7ffa36be1ace24e16aebddb2
Kevin Tinkler <kevint@boulder.net>
2016-03-14 16:58:45 -0600

    abstracting data out of rspec api spec file to data.yml

--- tests/api/recurly_account_api_rspec.rb
+++ tests/api/recurly_account_api_rspec.rb
@@ -8,30 +8,38 @@ require 'rubygems'
 require 'recurly'
 require 'rspec'
 require 'date'
+require 'yaml'
 
 describe "Recurly Accounts API" do
 
   before :all do
-    Recurly.subdomain         = 'kevint'
-    Recurly.api_key           = '21ce91e03831452794dca50790304a52'
-    Recurly.default_currency  = 'USD'
+    #Return nested hash of data from yaml file
+    @data = YAML.load_file("data/data.yml")
+    api_auth = @data["api_kevint"]
+
+    Recurly.subdomain         = api_auth["subdomain_kevint"]
+    Recurly.api_key           = api_auth["api_key_kevint"]
+    Recurly.default_currency  = api_auth["default_currency"]
     @unique_account_code      = DateTime.now.strftime('%s')
   end
 
   it "creates a new account" do
+    #Clean up first part of data hash reference
+    account_data = @data["api_account_1"]
+    
     account = Recurly::Account.create(
-      :account_code => @unique_account_code,
-      :email        => 'kevin_1@boulder.net',
-      :first_name   => 'Kevin_1',
-      :last_name    => 'One',
-      :company_name => 'ACME QA',
-      :address      => {
-        :address1   => '2666 W 119th Ave',
-        :city       => 'Westminster',
-        :state      => 'CO',
-        :zip        => '80234',
-        :country    => 'USA',
-        :phone      => '719-289-5539'
+    :account_code => @unique_account_code,
+    :email        => account_data["email"],
+    :first_name   => account_data["first_name"],
+    :last_name    => account_data["last_name"],
+    :company_name => account_data["company_name"],
+    :address      => {
+      :address1   => account_data["address1"],
+      :city       => account_data["city"],
+      :state      => account_data["state"],
+      :zip        => account_data["zip"],
+      :country    => account_data["country"],
+      :phone      => account_data["phone"]
      }
     )
     #confirm Account has been created successfully

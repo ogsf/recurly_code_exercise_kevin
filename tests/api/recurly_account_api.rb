2dbc0e628fd009e9507a114b1507e3fd32d5459d
Kevin Tinkler <kevint@boulder.net>
2016-03-14 16:37:38 -0600

    abstracting data to yml file

--- tests/api/recurly_account_api.rb
+++ tests/api/recurly_account_api.rb
@@ -1,46 +1,51 @@
 #
-# "This is not yet my final form."  This file is just me figuring out the Recurly API.
-# The Rspec version of this test is located at 'tests/api/recurly_account_api_rspec.rb'
+# This file is my initial POC effort while figuring out the Recurly API.
+# The Rspec test version is located at 'tests/api/recurly_account_api_rspec.rb'
 #
-# Run this here test from project root; 
+# Run this here file from project root; 
 # bundle exec ruby tests/api/recurly_account_api.rb
 #
 # - Kevin Tinkler 3/13/16
 #
 require 'rubygems'
 require 'recurly'
+require 'yaml'
 
-Recurly.subdomain         = 'kevint'
-Recurly.api_key           = '21ce91e03831452794dca50790304a52'
-Recurly.default_currency  = 'USD'
+@data = YAML.load_file("data/data.yml")
+api_auth = @data["api_kevint"]
+
+Recurly.subdomain         = api_auth["subdomain_kevint"]
+Recurly.api_key           = api_auth["api_key_kevint"]
+Recurly.default_currency  = api_auth["default_currency"]
 @unique_account_code      = DateTime.now.strftime('%s')
 
 begin
 
   #Create an account.
+  account_data = @data["api_account_1"]
   account = Recurly::Account.create(
     :account_code => @unique_account_code,
-    :email        => 'kevin_1@boulder.net',
-    :first_name   => 'Kevin_1',
-    :last_name    => 'One',
-    :company_name => 'ACME QA',
+    :email        => account_data["email"],
+    :first_name   => account_data["first_name"],
+    :last_name    => account_data["last_name"],
+    :company_name => account_data["company_name"],
     :address      => {
-      :address1   => '2666 W 119th Ave',
-      :city       => 'Westminster',
-      :state      => 'CO',
-      :zip        => '80234',
-      :country    => 'USA',
-      :phone      => '719-289-5539'
+      :address1   => account_data["address1"],
+      :city       => account_data["city"],
+      :state      => account_data["state"],
+      :zip        => account_data["zip"],
+      :country    => account_data["country"],
+      :phone      => account_data["phone"]
     }
   )
 
-  puts account.response.inspect  #Looking at the HTTP Status Code
-  puts #empty line to space output
+  puts account.response.inspect #Looking at the HTTP Status Code
+  puts                          #empty line to space output
 
   #Check if the new account exists
   #if account does not exist, a NotFound Error will be thrown
   account = Recurly::Account.find @unique_account_code
-  # account = Recurly::Account.find '54321'  #To test the NotFound Error
+  # account = Recurly::Account.find '54321'  #Uncomment to check the NotFound Error
   puts "Account: #{account.inspect}"
   rescue Recurly::Resource::NotFound => e
     puts e.message
